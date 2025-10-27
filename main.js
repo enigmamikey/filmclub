83ec8c4cec0896aa657888913cfae2982b6656d3
Michael Burgher <mikeburgher2@gmail.com>
2025-10-27 10:13:46 -0600

    global variables added including variables for most html elements

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('h1').textContent = 'Film Club'
+
+let sortedRounds
+const roundBtnsContainer = document.querySelector('#round-buttons-container')
+const roundTitle = document.querySelector('#round-title')
+const roundDataContainer = document.querySelector('#round-data-container')
+const rateBtn = document.querySelector('#rateBtn')
+const addMovieBtn = document.querySelector('#addMovieBtn')
