7c593429740d91b6c6672633c4ef8589b8f8c90a
Michael Burgher <mikeburgher2@gmail.com>
2026-02-09 10:29:24 -0700

    fixing log-in issues

--- auth.js
+++ auth.js
@@ -4,11 +4,13 @@ const SUPABASE_URL = 'https://bnsydsxrhzlyptwyvjll.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuc3lkc3hyaHpseXB0d3l2amxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwOTgzMzcsImV4cCI6MjA3NTY3NDMzN30.6isq1xIJS-y1opkixbP6CyX645uxsZGEBR0nkQJ3SEA'
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
+    flowType: 'implicit',          // <-- add this
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
   },
 });
+
 
 window.supabase = supabase
 
